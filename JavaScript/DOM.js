da5d1aa9f24b8c2a28dda50a8628716262e8804b
nathandinh <nathan.dinh03@outlook.com>
2023-12-18 18:17:34 -0500

    first message

---
+++ JavaScript/DOM.js
@@ -0,0 +1,11 @@
+function myFunctionIncreaseX2() {
+    document.getElementById("Summary").style.fontSize = "x-large";
+  }
+
+  function myFunctionIncreaseX1() {
+    document.getElementById("Summary").style.fontSize = "large";
+  }
+
+  function myFunctionNormal() {
+    document.getElementById("Summary").style.fontSize = "1em";
+  }
